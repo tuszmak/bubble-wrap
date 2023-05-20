0e93d9e16a18016085fe1b01025b883870373381
Dániel Pintér <daniel.pinter02@edu.bme.hu>
2023-05-20 21:29:12 +0200

    feat: place Sizeform to app

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,15 @@ function App() {
     width: 0,
     height: 0,
   });
- 
+  const handleSizeChange = (size: IPlaygroundSize) => {
+    setSize(size);
+  };
   return (
-    <div className="bg-blue-400 flex justify-center h-1">
-      Hi! This is the homepage!
-
+    <div className="bg-blue-400  h-screen">
+      <div className="flex justify-center">Hi! This is the homepage!</div>
+      <div className="p-20 flex justify-center items-center">
+        <SizeForm key="form" changer={handleSizeChange} />
+      </div>
     </div>
   );
 }
