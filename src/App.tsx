7d66d6945f4e13c67e607875a40e958afb394b7c
Dániel Pintér <daniel.pinter02@edu.bme.hu>
2023-05-20 21:16:58 +0200

    feat: create setter for playground size

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-
-import './App.css'
+import "./App.css";
+import { SizeForm } from "./components/SizeForm";
+import { useState } from "react";
+import { IPlaygroundSize } from "./types";
 
 function App() {
- return <div>Hi! This is the homepage!</div>
+  const [size, setSize] = useState<IPlaygroundSize>({
+    width: 0,
+    height: 0,
+  });
+ 
+  return (
+    <div className="bg-blue-400 flex justify-center h-1">
+      Hi! This is the homepage!
+
+    </div>
+  );
 }
 
-export default App
+export default App;
