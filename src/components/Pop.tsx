009906572657c1cbe9bf65ef0b3b9c8430683101
Dániel Pintér <daniel.pinter02@edu.bme.hu>
2023-06-21 13:42:51 +0200

    feat: implement sound

--- src/components/Pop.tsx
+++ src/components/Pop.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import popSFX from "../sounds/pop.mp3"
+
 
 export const Pop = () => {
+  const handleClick = ()=>{
+    setIsPopped(true)
+    new Audio(popSFX).play();
+  }
+  const [isPopped, setIsPopped] = useState<boolean>(false);
   return (
-    <div>Pop</div>
+    
+    isPopped ? 
+    <button className='btn'>Pop!</button>
+    :
+    <button className='btn w-16' onClick={handleClick}></button>
+
   )
 }
