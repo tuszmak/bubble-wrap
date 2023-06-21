5837c5fb1d177f91ad0f6e30148d1bf41b81fba8
tuszmak <daniel.pinter02@edu.bme.hu>
2023-06-21 14:07:50 +0200

    fix: make pop buttons consistent size

--- src/components/Pop.tsx
+++ src/components/Pop.tsx
@@ -3,15 +3,16 @@ import popSFX from "../sounds/pop.mp3"
 
 
 export const Pop = () => {
-  const handleClick = ()=>{
-    setIsPopped(true)
+  const handleClick = async()=>{
     new Audio(popSFX).play();
+      await new Promise(e => setTimeout(e, 110))
+      setIsPopped(true)
   }
   const [isPopped, setIsPopped] = useState<boolean>(false);
   return (
     
     isPopped ? 
-    <button className='btn'>Pop!</button>
+    <button className='btn w-16'>Pop!</button>
     :
     <button className='btn w-16' onClick={handleClick}></button>
 
